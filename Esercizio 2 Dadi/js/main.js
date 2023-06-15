c47c9f03ee171558212ecb436b22ac4c589441b1
marcocampisi <marco.campisi@outlook.it>
2023-06-15 18:26:52 +0200

    Added points cards

--- Esercizio 2 Dadi/js/main.js
+++ Esercizio 2 Dadi/js/main.js
@@ -1,13 +1,12 @@
-const dadoUtente = Math.floor(Math.random() * 6);
-const dadoComputer = Math.floor(Math.random() * 6);
 const bottoneGioco = document.getElementById('bottoneGioco');
 const punteggioComputer = document.getElementById('punteggioComputer');
 const punteggioUtente = document.getElementById('punteggioUtente');
 
 bottoneGioco.addEventListener('click',
     function(event) {
+        let dadoUtente = Math.floor(Math.random() * 6);
+        let dadoComputer = Math.floor(Math.random() * 6);
         punteggioUtente.innerHTML = dadoUtente;
         punteggioComputer.innerHTML = dadoComputer;
-        
     }
 );
