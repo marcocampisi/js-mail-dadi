f71bb82bc4774658aa186b74255ef45c4318594a
marcocampisi <marco.campisi@outlook.it>
2023-06-15 18:30:46 +0200

    Added result message (completed)

--- Esercizio 2 Dadi/js/main.js
+++ Esercizio 2 Dadi/js/main.js
@@ -8,5 +8,13 @@ bottoneGioco.addEventListener('click',
         let dadoComputer = Math.floor(Math.random() * 6);
         punteggioUtente.innerHTML = dadoUtente;
         punteggioComputer.innerHTML = dadoComputer;
+
+        if (dadoUtente > dadoComputer) {
+            risultato.innerHTML = "Hai vinto!";
+          } else if (dadoUtente < dadoComputer) {
+            risultato.innerHTML = "Hai perso!";
+          } else {
+            risultato.innerHTML = "Pareggio!";
+          }
     }
 );
