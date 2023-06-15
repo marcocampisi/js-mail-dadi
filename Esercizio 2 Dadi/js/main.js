2b642439abad3443b7d7c8e726e3f109d0e11102
marcocampisi <marco.campisi@outlook.it>
2023-06-15 17:03:37 +0200

    Added button game function

--- Esercizio 2 Dadi/js/main.js
+++ Esercizio 2 Dadi/js/main.js
@@ -1,3 +1,15 @@
 const dadoUtente = Math.floor(Math.random() * 6);
 const dadoComputer = Math.floor(Math.random() * 6);
+const bottoneGioco = document.getElementById('bottoneGioco');
 
+let gioco = false;
+
+bottoneGioco.addEventListener('click',
+    function(event) {
+        gioco = true;
+    }
+);
+
+if (gioco) {
+    
+}
