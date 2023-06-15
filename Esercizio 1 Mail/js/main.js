46c14a690fda3ba7b0fda00d57fadb791196adf2
marcocampisi <marco.campisi@outlook.it>
2023-06-15 15:25:08 +0200

    Added form structure

---
+++ Esercizio 1 Mail/js/main.js
@@ -0,0 +1,15 @@
+const mailAutorizzate = [
+    'pippo@boolean.careers', 
+    'marco@boolean.careers', 
+    'utente1@boolean.careers', 
+    'utente2@boolean.careers', 
+    'utente3@boolean.careers', 
+    'utente4@boolean.careers', 
+    'utente5@boolean.careers'
+];
+
+const mailUtente = document.getElementById('mailUtente').value
+
+for (let i = 0; i < mailAutorizzate.length; i++) {
+    console.log(mailAutorizzate[i]);
+}
